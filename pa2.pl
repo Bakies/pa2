e273d15b6a1288ecffacc7d3caee702ad06c98a3
end user <kreimendahlf@wit.edu>
2016-11-08 12:51:08 -0500

    added assignment description and submission file

---
+++ pa2.pl
@@ -0,0 +1,32 @@
+% COMP3071 - PA2
+
+when(1000,10).
+when(1200,12).
+when(3400,11).
+when(3350,12).
+when(2350,11).
+where(1000,dobbs102).
+where(1200,dobbs118).
+where(3400,wentw216).
+where(3350,wentw118).
+where(2350,wentw216).
+enroll(mary,1200).
+enroll(john,3400).
+enroll(mary,3350).
+enroll(john,1000).
+enroll(jim,1000).
+
+
+/************************************
+*****                           *****
+***** In your final submission, *****
+***** leave the text above this *****
+*****    comment untouched.     *****
+*****                           *****
+************************************/
+
+
+
+/*************************************
+** define your four predicates here **
+*************************************/
